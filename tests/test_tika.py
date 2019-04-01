75aa7e2cb6dcbcfdc5ee0b81d3ba6c985192441c
Paul Mooney <30876065+paultimothymooney@users.noreply.github.com>
2019-03-31 20:13:00 -0600

    Update test_tika.py

--- tests/test_tika.py
+++ tests/test_tika.py
@@ -15,16 +15,16 @@ class CreateTest(unittest.TestCase):
     #"test for file types"
     def test_remote_pdf(self):
         #'parse remote PDF'
-        self.assertTrue(tika.parser.from_file('/input/tests/data/tikatest1.pdf'))
+        self.assertTrue(tika.parser.from_file(filename='/input/tests/data/tikatest1.pdf'))
     def test_remote_html(self):
         #'parse remote HTML' 
-        self.assertTrue(tika.parser.from_file('/input/tests/data/tikatest2.htm'))
+        self.assertTrue(tika.parser.from_file(filename='/input/tests/data/tikatest2.htm'))
     def test_remote_mp3(self):
         #'parese remote mp3'
-        self.assertTrue(tika.parser.from_file('/input/tests/data/tikatest3.mp3'))
+        self.assertTrue(tika.parser.from_file(filename='/input/tests/data/tikatest3.mp3'))
     def test_remote_jpg(self):
         #'parse remote jpg'
-        self.assertTrue(tika.parser.from_file('/input/tests/data/tikatest4.jpg'))
+        self.assertTrue(tika.parser.from_file(filename='/input/tests/data/tikatest4.jpg'))
 
 if __name__ == '__main__':
     unittest.main()
