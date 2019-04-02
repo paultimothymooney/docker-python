e04af2c34cebd4be456906df8aee4da4ea4017c6
Paul Mooney <30876065+paultimothymooney@users.noreply.github.com>
2019-04-02 10:24:36 -0600

    Update test_tika.py

--- tests/test_tika.py
+++ tests/test_tika.py
@@ -1,13 +1,13 @@
 import unittest
 import tika
 from tika import parser
-#tika.TikaClientOnly = True
+tika.TikaClientOnly = True
 
-#class TestTika(unittest.TestCase):
-#    def test_tika(self):
-#        image = '/input/tests/data/tikatest.jpg'
-#        tika.initVM()
-#        parsed = parser.from_file(image)
+class TestTika(unittest.TestCase):
+    def test_tika(self):
+        image = '/input/tests/data/tikatest.jpg'
+        tika.initVM()
+        parsed = parser.from_file(image)
 
 # below from /chrismattmann/tika-python/tika/tests/test_tika.py
 #import unittest
@@ -16,16 +16,16 @@ from tika import parser
 #    #"test for file types"
 #    def test_remote_pdf(self):
 #        #'parse remote PDF'
-#        self.assertTrue(tika.parser.from_file(filename='/input/tests/data/tikatest1.pdf'))
+#        self.assertTrue(tika.parser.from_file('/input/tests/data/tikatest1.pdf'))
 #    def test_remote_html(self):
 #        #'parse remote HTML' 
-##        self.assertTrue(tika.parser.from_file(filename='/input/tests/data/tikatest2.htm'))
+##        self.assertTrue(tika.parser.from_file('/input/tests/data/tikatest2.htm'))
 # #   def test_remote_mp3(self):
 #        #'parese remote mp3'
-#        self.assertTrue(tika.parser.from_file(filename='/input/tests/data/tikatest3.mp3'))
+#        self.assertTrue(tika.parser.from_file('/input/tests/data/tikatest3.mp3'))
 #    def test_remote_jpg(self):
 #        #'parse remote jpg'
-#        self.assertTrue(tika.parser.from_file(filename='/input/tests/data/tikatest4.jpg'))#
+#        self.assertTrue(tika.parser.from_file('/input/tests/data/tikatest4.jpg'))#
 #
 #if __name__ == '__main__':
 #    unittest.main()
